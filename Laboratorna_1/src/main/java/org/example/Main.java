65a74264359fbbc39fb830210a91fdf893f6b176
Valeria Baranivska <v.baranivska.km23@kpi.ua>
2024-09-13 22:52:20 +0300

    Дописати конвертування всього списку

--- Laboratorna_1/src/main/java/org/example/Main.java
+++ Laboratorna_1/src/main/java/org/example/Main.java
@@ -7,20 +7,27 @@ public class Main {
         System.out.println("\t Баранівська Валерія KM-23\n");
 
         ArrayList<String> stringList = new ArrayList<>();
-        stringList.add("11");
+        stringList.add("50.01");
         stringList.add("22.5");
+        stringList.add("219.43564789456789090843726541253647895061231435466587098997676455242");
         stringList.add("46");
         stringList.add("32.98");
         stringList.add("97");
         stringList.add("100.9");
         stringList.add("312.9462");
-        stringList.add("1");
-        //stringList.add("2193876146278190429847634789234.43564789509084372654125364789506");
+        stringList.add("83");
+        stringList.add("21938761.4356478945678909084372654125364789506");
+        stringList.add("0.435623232323678909084372654125364789506");
 
-        System.out.println("\n \t Основна частина\n");
+
+
+        System.out.println("\n\t------> Основна частина\n");
         NumberOperations.print_num(stringList);
 
-        System.out.println("\n \t Друга частина \n");
+        System.out.println("\n\t------> Друга частина \n");
         NumberOperations.task_num(stringList);
+
+        System.out.println("\n\t------> Арифметичні дії з BigDecimal \n");
+        NumberOperations.additional_task(stringList);
             }
         }
--- Laboratorna_1/src/main/java/org/example/NumberOperations.java
+++ Laboratorna_1/src/main/java/org/example/NumberOperations.java
@@ -1,70 +1,86 @@
 package org.example;
 
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.ArrayList;
 import java.math.BigDecimal;
 
 public class NumberOperations {
-    public static void print_num(ArrayList<String> list_numbers){
+    public static void print_num(ArrayList<String> list_numbers) {
         List<Integer> intNum_list = new ArrayList<>();
         List<String> double_list = new ArrayList<>();
 
-        for(String number : list_numbers){
-            //конвертація всього списку до одного типу
+        for (int i = 0; i <list_numbers.size(); ++i ) {
+
             try {
-                int intNum = Integer.parseInt(number.split("\\.")[0]);
-                intNum_list.add(intNum);
-                double doubNum = Double.parseDouble(number);
-                double_list.add(String.format("%.2f", doubNum));
-            } catch (NumberFormatException e){
-                System.out.println("Помилка форматування числа: " + number);
+                // Перевірка, чи рядок є цілим числом
+                int intNum = Integer.parseInt(list_numbers.get(i));
+                intNum_list.add(Integer.valueOf(intNum));
+            } catch (NumberFormatException e) {
+                // Якщо не вдалося конвертувати в int, спробуємо конвертувати в double
+                try {
+                    double doubNum = Double.parseDouble(list_numbers.get(i));
+                    intNum_list.add((Integer.parseInt(String.valueOf(doubNum))));
+                    double_list.add(String.format("%.2f", doubNum).replace(",", "."));
+                } catch (NumberFormatException ex) {
+                    // Якщо не вдалося конвертувати в double, виведемо помилку
+                    System.out.println("Помилка форматування числа: " + list_numbers.get(i));
+                }
             }
         }
+            System.out.println("Всі числа: " + list_numbers);
+            System.out.println("Всі числа у форматі цілих чисел: " + intNum_list);
+            System.out.println("Всі числа у форматі дробових чисел: " + double_list);
+        }
 
-        System.out.println("Всі числа: " + list_numbers );
-        System.out.println("Всі числа у форматі цілих чисел: " + intNum_list);
-        System.out.println("Всі числа у форматі дробних чисел: " + double_list);
-
-        //System.out.println("Числа у форматі цілих чисел: " + integerNumbers);
-        //System.out.println("Числа у форматі дробних чисел: " + decimalNumbers); //з 2ма знаками після коми
-    }
-    public static void task_num(ArrayList<String> list_numbers){
+    public static void task_num(ArrayList<String> list_numbers) {
         List<BigDecimal> bigDecimalNumbers = new ArrayList<>();
 
         // Перетворення чисел на BigDecimal і додавання до списку
         for (String number : list_numbers) {
-            BigDecimal bd = new BigDecimal(number); //конструктор
-            bigDecimalNumbers.add(bd);
+            try {
+                BigDecimal num = new BigDecimal(number);
+                bigDecimalNumbers.add(num);
+            } catch (NumberFormatException e) {
+                System.out.println("Некоректний формат числа: " + e.getMessage());
+            }
         }
-        System.out.println("Числа у форматі BigDecimal: " + bigDecimalNumbers); //1
+        System.out.println("Числа у форматі BigDecimal: " + bigDecimalNumbers);
 
-        int sum_all = 0;
-        int product = 1;
+        BigDecimal sum_all = BigDecimal.ZERO;
+        BigDecimal product = BigDecimal.ONE;
+        int count = 0;
 
         List<Integer> integerNumbers = new ArrayList<>();
         List<Double> decimalNumbers = new ArrayList<>();
         List<String> Num2 = new ArrayList<>();
 
-        for ( String number : list_numbers){
-            if (number.matches("^\\d*\\.\\d+$")) {
-                double decimalNumber = Double.parseDouble(number);
+        for (BigDecimal number : bigDecimalNumbers) {
+            // Перевіряємо, чи число є дробовим або цілим
+            if (number.scale() > 0) { // Число є дробовим
+                double decimalNumber = number.doubleValue();
                 decimalNumbers.add(decimalNumber);
-                sum_all += decimalNumber;
-                product *= decimalNumber;
-                double prod_num = decimalNumber * 2;
-                Num2.add(String.valueOf(prod_num));
-            }
-            else if (number.matches("\\d+")) {
-                int integerNumber = Integer.parseInt(number);
+                sum_all = sum_all.add(number);
+                if (count < 5) {
+                    product = product.multiply(number);
+                    count++;
+                }
+
+            } else { // Число є цілим
+                int integerNumber = number.intValue();
                 integerNumbers.add(integerNumber);
-                sum_all += integerNumber;
-                product *= integerNumber;
-                double prod_num = integerNumber * 2;
-                Num2.add(String.valueOf(prod_num));
+                sum_all = sum_all.add(number);
+                if (count < 5) {
+                    product = product.multiply(number);
+                    count++;
+                }
             }
+            BigDecimal prod_num = number.multiply(BigDecimal.valueOf(2));
+            Num2.add(String.valueOf(prod_num));
         }
         int totalNumbers = integerNumbers.size() + decimalNumbers.size();
-        double average = (totalNumbers > 0) ? (double) sum_all / totalNumbers : 0; // Запобігання діленню на нуль
+        BigDecimal totalNumbersBD = BigDecimal.valueOf(totalNumbers);
+        BigDecimal average = (totalNumbers > 0) ? sum_all.divide(totalNumbersBD, RoundingMode.HALF_UP) : BigDecimal.ZERO;
 
         System.out.println("Сума всіх чисел в списку: " + sum_all);
         System.out.println("Добуток перших 5 чисел списку: " + product);
@@ -73,7 +89,7 @@ public class NumberOperations {
 
         int sum_pow = 0;
         int sum_50 = 0;
-        for(Integer num : integerNumbers) {
+        for (Integer num : integerNumbers) {
             int pow_num = (int) Math.pow(num, 2);
             sum_pow += pow_num;
             if (num > 50) {
@@ -81,24 +97,49 @@ public class NumberOperations {
             }
         }
         System.out.println("Сума квадратів нового списку: " + sum_pow);
-        System.out.println("Сума всіх чисел, які більші 50: "+ sum_50);
+        System.out.println("Сума всіх чисел, які більші 50: " + sum_50);
 
         List<String> formula_num = new ArrayList<>();
-        for(String number : Num2) {
-            if (number.matches("^\\d*\\.\\d+$")) {
-                double decimalNumber = Double.parseDouble(number);
+        for (String numberStr : Num2) {
+            BigDecimal number = new BigDecimal(numberStr);
+
+            if (number.scale() > 0) { // Число є дробовим
+                double decimalNumber = number.doubleValue();
                 decimalNumbers.add(decimalNumber);
-                double f_num = (decimalNumber + 10) / 2;
-                formula_num.add(String.format("%.2f",f_num));
-            }
-            else if (number.matches("\\d+")) {
-                int integerNumber = Integer.parseInt(number);
-                integerNumbers.add(integerNumber);
-                int f_num = (integerNumber + 10) / 2;
+
+                // Формула: (decimalNumber + 10) / 2
+                BigDecimal f_num = number.add(BigDecimal.TEN).divide(BigDecimal.valueOf(2), RoundingMode.HALF_UP);
+                formula_num.add(f_num.toString());
+
+            } else { // Число є цілим
+                int integerNumber = number.intValue();
+
+                // Формула: (decimalNumber + 10) / 2
+                int f_num = (10 + integerNumber) / 2;
                 formula_num.add(String.valueOf(f_num));
             }
         }
-        System.out.println("Новий список, де кожне число початкового помножене на 2: " + Num2 );
-        System.out.println("Формула до кожного числа:" + formula_num );
+        System.out.println("Новий список, де кожне число початкового помножене на 2: " + Num2);
+        System.out.println("Формула до кожного числа:" + formula_num);
+    }
+    public static void additional_task(ArrayList<String> list_numbers) {
+        List<BigDecimal> bigDecimalNumbers = new ArrayList<>();
+
+        // Перетворення чисел на BigDecimal і додавання до списку
+        for (String number : list_numbers) {
+            try {
+                BigDecimal num = new BigDecimal(number);
+                bigDecimalNumbers.add(num);
+            } catch (NumberFormatException e) {
+                System.out.println("Некоректний формат числа: " + e.getMessage());
+            }
+        }
+
+        System.out.println("Числа у форматі BigDecimal: " + bigDecimalNumbers);
+
+        BigDecimal add_num = bigDecimalNumbers.get(1).add(bigDecimalNumbers.get(9));
+        System.out.println("BigDecimalAdd(2 і 10 елементи) : " + add_num);
+        BigDecimal mult_num = bigDecimalNumbers.get(0).multiply(bigDecimalNumbers.get(5));
+        System.out.println("BigDecimalMultiply ( 1 і 6 елементи) : " + mult_num);
     }
 }
